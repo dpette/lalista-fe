706844cb3dc6eaa187dbac908066270a048a27df
Daniel <pettenon.daniel@gmail.com>
2018-02-12 12:27:57 +0100

    removed chart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,10 @@ import { PointsComponent } from './points/points.component';
 import { NewPointComponent } from './points/new-point/new-point.component';
 import { WordsComponent } from './words/words.component';
 import { PeopleComponent } from './people/people.component';
-import { ChartComponent } from './chart/chart.component';
 import { AppComponent } from './app.component';
 
 const appRoutes: Routes = [
-  { path: '', component: ChartComponent, pathMatch: 'full' },
+  // { path: '', component: , pathMatch: 'full' },
   { path: 'new-point', component: NewPointComponent },
   { path: 'points', component: PointsComponent },
   { path: 'people', component: PeopleComponent },
