9c0385b2034e49de8ad20ed20ed767a7ab4da954
Daniel <pettenon.daniel@gmail.com>
2018-03-31 00:11:30 +0200

    app scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
+import { Router, NavigationEnd } from '@angular/router';
 import { environment } from './../environments/environment';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import {Angular2TokenService} from 'angular2-token';
 
@@ -8,11 +9,20 @@ import {Angular2TokenService} from 'angular2-token';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
 
-  constructor(private authToken: Angular2TokenService) {
+  constructor(private router: Router, private authToken: Angular2TokenService) {
     this.authToken.init({ apiBase: environment.apiUrl });
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+          return;
+      }
+      window.scrollTo(0, 0);
+  });
   }
 
 }
